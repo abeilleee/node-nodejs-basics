e1f7fb0b17622ebd93e40bf85933fc75ebeb6115
Mayya <abeillee@bk.ru>
2025-10-21 19:37:31 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import path from "path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const TARGET_FOLDER = "files";
+const TARGET_FILE = "fileToRead.txt";
+const ERROR_MSG = "FS operation failed";
+const NO_ENTRY = "ENOENT";
+const TARGET_PATH = path.join(__dirname, TARGET_FOLDER, TARGET_FILE);
+
 const read = async () => {
-  // Write your code here
+    try {
+        const fileContent = await fs.readFile(TARGET_PATH, {
+            encoding: "utf8",
+        });
+        console.log(fileContent);
+    } catch (error) {
+        if (error.code === NO_ENTRY) {
+            throw new Error(ERROR_MSG);
+        }
+
+        console.error("Error occured: ", error);
+    }
 };
 
 await read();
