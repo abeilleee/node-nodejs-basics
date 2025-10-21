9a9841725291fdad0403201830425a97c905d6a1
Mayya <abeillee@bk.ru>
2025-10-21 19:31:37 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import path from "path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const TARGET_FOLDER = "files";
+const TARGET_PATH = path.join(__dirname, TARGET_FOLDER);
+const ERROR_MSG = "FS operation failed";
+const NO_ENTRY = "ENOENT";
+
 const list = async () => {
-  // Write your code here
+    try {
+        const filesNames = await fs.readdir(TARGET_PATH);
+        console.log(filesNames);
+    } catch (error) {
+        if (error.code === NO_ENTRY) {
+            throw new Error(ERROR_MSG);
+        }
+
+        console.error("Error occured: ", error);
+    }
 };
 
 await list();
