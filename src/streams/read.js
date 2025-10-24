4a7f7b31d5bd17ae0e1aa5be8c721dc60da7a39c
Mayya <abeillee@bk.ru>
2025-10-24 22:06:43 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import path from "path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const TARGET_FOLDER = "files";
+const TARGET_FILE = "fileToRead.txt";
+const FILE_PATH = path.join(__dirname, TARGET_FOLDER, TARGET_FILE);
+
 const read = async () => {
-  // Write your code here
+    const stream = fs.createReadStream(FILE_PATH);
+
+    stream.on("data", (chunk) => {
+        process.stdout.write(`${chunk}\n)`);
+    });
+
+    stream.on("error", (error) => {
+        console.error(error);
+    });
 };
 
 await read();
